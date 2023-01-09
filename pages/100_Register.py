734735cfa0b769ff767508a40b17a0e3251e63f5
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-09 21:02:17 +0100

    opti of register page

--- pages/100_Register.py
+++ pages/100_Register.py
@@ -6,6 +6,27 @@ title = "Register"
 st.set_page_config(page_title=title, page_icon="🗒️")
 st.title(title)
 
+def regPage():
+    # main
+    regContainer = st.container()
+    name = regContainer.text_input("Name", "")
+    username = regContainer.text_input("Username", "")
+    password = regContainer.text_input("Password", type="password")
+
+    # get all usernames to test if it already exists
+    users = _auth.fetchAllUsers()
+    usernames = [user["key"] for user in users]
+
+    if regContainer.button("Register"):
+        if name == "" or username == "" or password == "":
+            st.error("Please fill all the fields")
+        elif username in usernames:
+            st.error("Username already exists")
+        else:
+            st.success("Registered !")
+            st.info("You can now login at the home page")
+            _auth.insertUser(name, username, password)
+
 
 try:
     if st.session_state.authentication_status == True:
@@ -13,42 +34,6 @@ try:
         st.session_state.authenticator.logout("Logout", "sidebar")
 
     if st.session_state.authentication_status == None or st.session_state.authentication_status == False:
-        # main
-        regContainer = st.container()
-        name = regContainer.text_input("Name", "")
-        username = regContainer.text_input("Username", "")
-        password = regContainer.text_input("Password", type="password")
-
-        # get all usernames to test if it already exists
-        users = _auth.fetchAllUsers()
-        usernames = [user["key"] for user in users]
-
-        if regContainer.button("Register"):
-            if name == "" or username == "" or password == "":
-                st.error("Please fill all the fields")
-            elif username in usernames:
-                st.error("Username already exists")
-            else:
-                st.success("Registered !")
-                st.info("You can now login at the home page")
-                _auth.insertUser(name, username, password)
+        regPage()
 except:
-        #main
-        regContainer = st.container()
-        name = regContainer.text_input("Name", "")
-        username = regContainer.text_input("Username", "")
-        password = regContainer.text_input("Password", type="password")
-
-        # get all usernames to test if it already exists
-        users = _auth.fetchAllUsers()
-        usernames = [user["key"] for user in users]
-
-        if regContainer.button("Register"):
-            if name == "" or username == "" or password == "":
-                st.error("Please fill all the fields")
-            elif username in usernames:
-                st.error("Username already exists")
-            else:
-                st.success("Registered !")
-                st.info("You can now login at the home page")
-                _auth.insertUser(name, username, password)
+    regPage()
