4a21dfc4433da2dbc05a5882879666d8145d91c2
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-09 17:44:06 +0100

    better scrambles algo

--- getScrambles.py
+++ getScrambles.py
@@ -4,6 +4,10 @@ moves = ("U", "U'", "U2", "D", "D'", "D2", "L", "L'", "L2", "R", "R'", "R2", "F"
 
 def getScramble(times=15):
     scramble = ""
+    lastMove = ""
     for i in range(times):
-        scramble += random.choice(moves) + " "
+        move = random.choice(moves)
+        if move != lastMove:
+            scramble += move + " "
+            lastMove = move
     return scramble
