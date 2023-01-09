bd7abb3ea729016c14ef6ec6fc0b3c32a8c5c673
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-09 17:18:18 +0100

    error if user already exists

--- pages/100_Register.py
+++ pages/100_Register.py
@@ -6,14 +6,17 @@ title = "Register"
 st.set_page_config(page_title=title, page_icon="🗒️")
 st.title(title)
 
+# get all usernames to test if it already exists
+users = _auth.fetchAllUsers()
+usernames = [user["key"] for user in users]
+
 try:
     if st.session_state.authentication_status == True:
         st.error("You are already logged in")
         st.session_state.authenticator.logout("Logout", "sidebar")
 
     if st.session_state.authentication_status == None or st.session_state.authentication_status == False:
-
-        #main
+        # main
         regContainer = st.container()
         name = regContainer.text_input("Name", "")
         username = regContainer.text_input("Username", "")
@@ -22,6 +25,8 @@ try:
         if regContainer.button("Register"):
             if name == "" or username == "" or password == "":
                 st.error("Please fill all the fields")
+            elif username in usernames:
+                st.error("Username already exists")
             else:
                 st.success("Registered !")
                 st.info("You can now login at the home page")
@@ -36,6 +41,8 @@ except:
         if regContainer.button("Register"):
             if name == "" or username == "" or password == "":
                 st.error("Please fill all the fields")
+            elif username in usernames:
+                st.error("Username already exists")
             else:
                 st.success("Registered !")
                 st.info("You can now login at the home page")
