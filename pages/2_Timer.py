eacafd3b79c69edbe6a1f369a4f57033fa9195b3
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-09 17:35:50 +0100

    bug fix

--- pages/2_Timer.py
+++ pages/2_Timer.py
@@ -5,22 +5,18 @@ title = "Timer"
 st.set_page_config(page_title=title, page_icon="⏱️")
 st.title(title)
 
-try:
-    if st.session_state.authentication_status == None or st.session_state.authentication_status == False:
-        st.error("Please login to access this page")
+if st.session_state.authentication_status == None or st.session_state.authentication_status == False:
+    st.error("Please login to access this page")
 
-    if st.session_state.authentication_status == True:
-        st.session_state.authenticator.logout("Logout", "sidebar")
+if st.session_state.authentication_status == True:
+    st.session_state.authenticator.logout("Logout", "sidebar")
 
-        #main
-        scrambleContainer = st.empty
-        scrambleContainer.subheader(getScramble())
+    #main
+    scrambleContainer = st.empty
+    scrambleContainer.subheader(getScramble())
 
-        scrambleSizeCol, rescrambleCol = st.columns(2)
-        scrambleSizeOption = scrambleSizeCol.selectbox("Scramble size", ["15", "20", "25", "30"], key="scrambleSizeOption")
+    scrambleSizeCol, rescrambleCol = st.columns(2)
+    scrambleSizeOption = scrambleSizeCol.selectbox("Scramble size", ["15", "20", "25", "30"], key="scrambleSizeOption")
 
-        if rescrambleCol.button("Rescramble"):
-            scrambleContainer.subheader(getScramble(int(scrambleSizeOption)))
-        
-except:
-    st.error("Please go to home page first")
+    if rescrambleCol.button("Rescramble"):
+        scrambleContainer.subheader(getScramble(int(scrambleSizeOption)))
