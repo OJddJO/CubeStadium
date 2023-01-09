bf34fd47d78fc1ad3fedba8a3486a9a4760682e1
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-09 17:51:20 +0100

    bug fix

--- getScrambles.py
+++ getScrambles.py
@@ -4,7 +4,7 @@ moves = ("U", "U'", "U2", "D", "D'", "D2", "L", "L'", "L2", "R", "R'", "R2", "F"
 
 def getScramble(times=15):
     scramble = ""
-    lastMove = ""
+    lastMove = " "
     while len(scramble.split(sep=" ")) <= times:
         move = random.choice(moves)
         if move != lastMove[0]:
