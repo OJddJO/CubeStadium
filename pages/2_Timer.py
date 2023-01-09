4fb11f2f92c825f6229482e2df23e817eb9ba2a9
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-09 19:31:27 +0100

    test

--- pages/2_Timer.py
+++ pages/2_Timer.py
@@ -32,23 +32,24 @@ try:
         timerContainer = st.empty()
         st.session_state.timer = 0.00
         st.session_state.timerStarted = False
-        timerContainer.subheader(str(st.session_state.timer).format("0.3f"))
+        timerContainer.subheader("{:.2f}".format(st.session_state.timer))
 
         async def timerFunc():
+            buttonState = button.button("Stop")
             st.session_state.timer = 0.00
             while st.session_state.timerStarted:
                 st.session_state.timer += 0.01
-                timerContainer.subheader(str(st.session_state.timer).format("%.3f"))
+                timerContainer.subheader("{:.2f}".format(st.session_state.timer))
                 await asyncio.sleep(0.01)
+                if buttonState:
+                    st.session_state.timerStarted = False
+                    break
         
-        startButton, stopButton = st.columns(2)
-        if startButton.button("Start"):
+        button = st.empty()
+        if button.button("Start"):
             if st.session_state.timerStarted == False:
                 st.session_state.timerStarted = True
                 asyncio.run(timerFunc())
-
-        if stopButton.button("Stop"):
-            st.session_state.timerStarted = False
 
 
 except Exception as e:
