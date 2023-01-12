94370561fb849f2d545090dea2f88f2cc779b8b5
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-12 18:42:51 +0100

    better ressource page

--- pages/4_Ressources.py
+++ pages/4_Ressources.py
@@ -6,18 +6,18 @@ st.sidebar.markdown("**Made with ❤️ by** [***OJddJO***](https://github.com/O
 
 algo = st.expander("Algorithmes")
 algo.subheader("3x3x3")
-algo.caption("F2L: https://jperm.net/3x3/cfop/f2l")
-algo.caption("OLL: https://jperm.net/algs/oll")
-algo.caption("PLL: https://jperm.net/algs/pll")
-algo.caption("Coll: https://jperm.net/algs/coll")
-algo.caption("Winter Variation: https://jperm.net/algs/wv")
-algo.caption("Advanced F2L PDF: https://drive.google.com/file/d/1nzAXYUWZJ6H2wIOXaHdWXep3W57tArbR/view")
-algo.caption("CubeHead PLL PDF: https://drive.google.com/file/d/1JM798XJ1FlpRF-8oGlil8l2-57axbMXn/view")
+algo.caption("- F2L: https://jperm.net/3x3/cfop/f2l")
+algo.caption("- OLL: https://jperm.net/algs/oll")
+algo.caption("- PLL: https://jperm.net/algs/pll")
+algo.caption("- COLL: https://jperm.net/algs/coll")
+algo.caption("- Winter Variation: https://jperm.net/algs/wv")
+algo.caption("- Advanced F2L PDF: https://drive.google.com/file/d/1nzAXYUWZJ6H2wIOXaHdWXep3W57tArbR/view")
+algo.caption("- CubeHead PLL PDF: https://drive.google.com/file/d/1JM798XJ1FlpRF-8oGlil8l2-57axbMXn/view")
 
 others = st.expander("Others")
 others.subheader("3x3x3")
-others.caption("2-Sided PLL Recognition: https://logiqx.github.io/cubing-algs/html/2spll.html")
-others.caption("PLL Trainer: https://speedcubedb.com/t/pllrecog")
+others.caption("- 2-Sided PLL Recognition: https://logiqx.github.io/cubing-algs/html/2spll.html")
+others.caption("- PLL Trainer: https://speedcubedb.com/t/pllrecog")
 
 try:
     st.session_state.authenticator.logout("Logout", "sidebar")
