92c7262d731c5c81ad8d179f2cd4c768eb3d269f
OJd_dJO <jeremyli2906@hotmail.com>
2023-01-12 23:27:36 +0100

    working rotation cube on matrix

--- extension/cubeModel.py
+++ extension/cubeModel.py
@@ -7,7 +7,6 @@ class Cube():
         self.cube[1] = [[4 for i in range(3)] for j in range(3)] #green FRONT
         self.cube[2] = [[1 for i in range(3)] for j in range(3)] #yellow DOWN
         self.cube[3] = [[3 for i in range(3)] for j in range(3)] #orange LEFT
-        self.cube[3] = [[1,2,3],[4,5,6],[7,8,9]]
         self.cube[4] = [[2 for i in range(3)] for j in range(3)] #red RIGHT
         self.cube[5] = [[5 for i in range(3)] for j in range(3)] #blue BACK
         self.scramble = scramble
@@ -16,33 +15,84 @@ class Cube():
     def rotateL(self):
         for i in range(3):
             self.cube[0][i][0], self.cube[1][i][0], self.cube[2][i][0], self.cube[5][i][2] = self.cube[5][i][2], self.cube[0][i][0], self.cube[1][i][0], self.cube[2][i][0]
-        #rotation of LEFT face 3 times clockwise
+        #rotation of LEFT face 2 times clockwise
         for i in range(2):
             self.cube[3][0][0], self.cube[3][0][1], self.cube[3][0][2], self.cube[3][1][2], self.cube[3][2][2], self.cube[3][2][1], self.cube[3][2][0], self.cube[3][1][0] = self.cube[3][1][0], self.cube[3][0][0], self.cube[3][0][1], self.cube[3][0][2], self.cube[3][1][2], self.cube[3][2][2], self.cube[3][2][1], self.cube[3][2][0]
 
     def rotateLp(self):
         for i in range(3):
             self.cube[0][i][0], self.cube[1][i][0], self.cube[2][i][0], self.cube[5][i][2] = self.cube[1][i][0], self.cube[2][i][0], self.cube[5][i][2], self.cube[0][i][0]
-        #rotation of LEFT face 3 times counter-clockwise
+        #rotation of LEFT face 2 times counter-clockwise
         for i in range(2):
             self.cube[3][0][0], self.cube[3][0][1], self.cube[3][0][2], self.cube[3][1][2], self.cube[3][2][2], self.cube[3][2][1], self.cube[3][2][0], self.cube[3][1][0] = self.cube[3][0][1], self.cube[3][0][2], self.cube[3][1][2], self.cube[3][2][2], self.cube[3][2][1], self.cube[3][2][0], self.cube[3][1][0], self.cube[3][0][0]
 
     def rotateR(self):
-        pass
+        for i in range(3):
+            self.cube[0][i][2], self.cube[1][i][2], self.cube[2][i][2], self.cube[5][i][0] = self.cube[1][i][2], self.cube[2][i][2], self.cube[5][i][0], self.cube[0][i][2]
+        #rotation of RIGHT face 2 times clockwise
+        for i in range(2):
+            self.cube[4][0][0], self.cube[4][0][1], self.cube[4][0][2], self.cube[4][1][2], self.cube[4][2][2], self.cube[4][2][1], self.cube[4][2][0], self.cube[4][1][0] = self.cube[4][1][0], self.cube[4][0][0], self.cube[4][0][1], self.cube[4][0][2], self.cube[4][1][2], self.cube[4][2][2], self.cube[4][2][1], self.cube[4][2][0]
+
+    def rotateRp(self):
+        for i in range(3):
+            self.cube[0][i][2], self.cube[1][i][2], self.cube[2][i][2], self.cube[5][i][0] = self.cube[5][i][0], self.cube[0][i][2], self.cube[1][i][2], self.cube[2][i][2]
+        #rotation of RIGHT face 2 times counter-clockwise
+        for i in range(2):
+            self.cube[4][0][0], self.cube[4][0][1], self.cube[4][0][2], self.cube[4][1][2], self.cube[4][2][2], self.cube[4][2][1], self.cube[4][2][0], self.cube[4][1][0] = self.cube[4][0][1], self.cube[4][0][2], self.cube[4][1][2], self.cube[4][2][2], self.cube[4][2][1], self.cube[4][2][0], self.cube[4][1][0], self.cube[4][0][0]
 
     def rotateU(self):
-        pass
+        for i in range(3):
+            self.cube[1][0][i], self.cube[3][0][i], self.cube[5][0][i], self.cube[4][0][i] = self.cube[4][0][i], self.cube[1][0][i], self.cube[3][0][i], self.cube[5][0][i]
+        #rotation of UP face 2 times clockwise
+        for i in range(2):
+            self.cube[0][0][0], self.cube[0][0][1], self.cube[0][0][2], self.cube[0][1][2], self.cube[0][2][2], self.cube[0][2][1], self.cube[0][2][0], self.cube[0][1][0] = self.cube[0][1][0], self.cube[0][0][0], self.cube[0][0][1], self.cube[0][0][2], self.cube[0][1][2], self.cube[0][2][2], self.cube[0][2][1], self.cube[0][2][0]
+    
+    def rotateUp(self):
+        for i in range(3):
+            self.cube[1][0][i], self.cube[3][0][i], self.cube[5][0][i], self.cube[4][0][i] = self.cube[3][0][i], self.cube[5][0][i], self.cube[4][0][i], self.cube[1][0][i]
+        #rotation of UP face 2 times counter-clockwise
+        for i in range(2):
+            self.cube[0][0][0], self.cube[0][0][1], self.cube[0][0][2], self.cube[0][1][2], self.cube[0][2][2], self.cube[0][2][1], self.cube[0][2][0], self.cube[0][1][0] = self.cube[0][0][1], self.cube[0][0][2], self.cube[0][1][2], self.cube[0][2][2], self.cube[0][2][1], self.cube[0][2][0], self.cube[0][1][0], self.cube[0][0][0]
 
     def rotateD(self):
-        pass
+        for i in range(3):
+            self.cube[1][2][i], self.cube[3][2][i], self.cube[5][2][i], self.cube[4][2][i] = self.cube[3][2][i], self.cube[5][2][i], self.cube[4][2][i], self.cube[1][2][i]
+        #rotation of DOWN face 2 times clockwise
+        for i in range(2):
+            self.cube[2][0][0], self.cube[2][0][1], self.cube[2][0][2], self.cube[2][1][2], self.cube[2][2][2], self.cube[2][2][1], self.cube[2][2][0], self.cube[2][1][0] = self.cube[2][1][0], self.cube[2][0][0], self.cube[2][0][1], self.cube[2][0][2], self.cube[2][1][2], self.cube[2][2][2], self.cube[2][2][1], self.cube[2][2][0]
+    
+    def rotateDp(self):
+        for i in range(3):
+            self.cube[1][2][i], self.cube[3][2][i], self.cube[5][2][i], self.cube[4][2][i] = self.cube[4][2][i], self.cube[1][2][i], self.cube[3][2][i], self.cube[5][2][i]
+        #rotation of DOWN face 2 times counter-clockwise
+        for i in range(2):
+            self.cube[2][0][0], self.cube[2][0][1], self.cube[2][0][2], self.cube[2][1][2], self.cube[2][2][2], self.cube[2][2][1], self.cube[2][2][0], self.cube[2][1][0] = self.cube[2][0][1], self.cube[2][0][2], self.cube[2][1][2], self.cube[2][2][2], self.cube[2][2][1], self.cube[2][2][0], self.cube[2][1][0], self.cube[2][0][0]
 
     def rotateF(self):
-        pass
+        for i in range(3):
+            self.cube[0][2][i], self.cube[3][i][2], self.cube[2][0][2-i], self.cube[4][i][0] = self.cube[3][i][2], self.cube[2][0][2-i], self.cube[4][i][0], self.cube[0][2][i]
+        #rotation of FRONT face 2 times clockwise
+        for i in range(2):
+            self.cube[1][0][0], self.cube[1][0][1], self.cube[1][0][2], self.cube[1][1][2], self.cube[1][2][2], self.cube[1][2][1], self.cube[1][2][0], self.cube[1][1][0] = self.cube[1][1][0], self.cube[1][0][0], self.cube[1][0][1], self.cube[1][0][2], self.cube[1][1][2], self.cube[1][2][2], self.cube[1][2][1], self.cube[1][2][0]
+    
+    def rotateFp(self):
+        for i in range(3):
+            self.cube[0][2][i], self.cube[3][i][2], self.cube[2][0][2-i], self.cube[4][i][0] = self.cube[4][i][0], self.cube[0][2][i], self.cube[3][i][2], self.cube[2][0][2-i]
+        #rotation of FRONT face 2 times counter-clockwise
+        for i in range(2):
+            self.cube[1][0][0], self.cube[1][0][1], self.cube[1][0][2], self.cube[1][1][2], self.cube[1][2][2], self.cube[1][2][1], self.cube[1][2][0], self.cube[1][1][0] = self.cube[1][0][1], self.cube[1][0][2], self.cube[1][1][2], self.cube[1][2][2], self.cube[1][2][1], self.cube[1][2][0], self.cube[1][1][0], self.cube[1][0][0]
 
     def rotateB(self):
-        pass
+        for i in range(3):
+            self.cube[0][0][i], self.cube[4][i][2], self.cube[2][2][2-i], self.cube[3][i][0] = self.cube[4][i][2], self.cube[2][2][2-i], self.cube[3][i][0], self.cube[0][0][i]
+        #rotation of BACK face 2 times clockwise
+        for i in range(2):
+            self.cube[5][0][0], self.cube[5][0][1], self.cube[5][0][2], self.cube[5][1][2], self.cube[5][2][2], self.cube[5][2][1], self.cube[5][2][0], self.cube[5][1][0] = self.cube[5][1][0], self.cube[5][0][0], self.cube[5][0][1], self.cube[5][0][2], self.cube[5][1][2], self.cube[5][2][2], self.cube[5][2][1], self.cube[5][2][0]
+    
+    def rotateBp(self):
+        for i in range(3):
+            self.cube[0][0][i], self.cube[4][i][2], self.cube[2][2][2-i], self.cube[3][i][0] = self.cube[3][i][0], self.cube[0][0][i], self.cube[4][i][2], self.cube[2][2][2-i]
+        #rotation of BACK face 2 times counter-clockwise
+        for i in range(2):
+            self.cube[5][0][0], self.cube[5][0][1], self.cube[5][0][2], self.cube[5][1][2], self.cube[5][2][2], self.cube[5][2][1], self.cube[5][2][0], self.cube[5][1][0] = self.cube[5][0][1], self.cube[5][0][2], self.cube[5][1][2], self.cube[5][2][2], self.cube[5][2][1], self.cube[5][2][0], self.cube[5][1][0], self.cube[5][0][0]
 
-cube = Cube("R U R' U'")
-print(cube.cube)
-cube.rotateL()
-print(cube.cube)
