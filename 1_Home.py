ea2a29f6f43f92a64e6a00febe503b98229ed83d
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-14 18:37:03 +0100

    database not found fix

--- 1_Home.py
+++ 1_Home.py
@@ -38,7 +38,6 @@ if authenticationStatus == True:
 
     #get user data
     st.session_state.userIndex = usernames.index(st.session_state.username)
-    st.info(st.session_state.username)
 
     pb = st.session_state.userDatas[st.session_state.userIndex]["pb"]
     ao5 = st.session_state.userDatas[st.session_state.userIndex]["ao5"]
--- extension/userData.py
+++ extension/userData.py
@@ -3,7 +3,7 @@ import deta
 
 conn = deta.Deta(os.environ["db_key"])
 
-db = conn.Base("data")
+db = conn.Base("cubestadium_users")
 
 def insertData(username, data={"pb": None, "ao5": None, "ao12": None, "scrambles": [], "times": [], "list_ao5": [], "list_ao12": []}):
     return db.put({"key": username, "data": data})
