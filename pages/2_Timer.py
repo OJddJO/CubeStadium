bcfaebd26470fa33c97aafe4a8d142871bee45f7
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-14 10:43:09 +0100

    show error

--- pages/2_Timer.py
+++ pages/2_Timer.py
@@ -112,5 +112,6 @@ try:
         st.session_state.authenticator.logout("Logout", "sidebar")
             
 
-except:
+except Exception as e:
     st.error("Please go to home page first")
+    st.error(e)
