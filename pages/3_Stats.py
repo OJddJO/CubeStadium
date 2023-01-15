8b5fc6a2ada84f2485b54426f35d3a43768f1d8c
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-15 13:15:50 +0100

    added stats page

---
+++ pages/3_Stats.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from PIL import Image
+
+title = "Stats"
+icon = Image.open("icon.png")
+st.set_page_config(page_title=title, page_icon=icon)
+st.title(title)
+st.sidebar.markdown("**Made with ❤️ by** [***OJddJO***](https://github.com/OJddJO/)")
+
+try:
+    if st.session_state.authentication_status == None or st.session_state.authentication_status == False:
+        st.error("Please login to access this page")
+
+    if st.session_state.authentication_status == True:
+        #sidebar
+        st.session_state.authenticator.logout("Logout", "sidebar")
+
+        #main
+        st.header("Coming soon !")
+
+        st.button("Refresh")
+
+        #data
+        pb = st.session_state.userDatas[st.session_state.userIndex]["pb"]
+        ao5 = st.session_state.userDatas[st.session_state.userIndex]["ao5"]
+        ao12 = st.session_state.userDatas[st.session_state.userIndex]["ao12"]
+        scrambles = st.session_state.userDatas[st.session_state.userIndex]["scrambles"]
+        times = st.session_state.userDatas[st.session_state.userIndex]["times"]
+        list_ao5 = st.session_state.userDatas[st.session_state.userIndex]["list_ao5"]
+        list_ao12 = st.session_state.userDatas[st.session_state.userIndex]["list_ao12"]
+
+        col1, col2, col3, = st.columns(3)
+
+        col1.metric("Personnal Best:", pb)
+        col2.metric("Best average of 5:", ao5)
+        col3.metric("Best average of 12:", ao12)
+
+        with st.expander("All times"):
+            for element in times:
+                st.write(element, scrambles[times.index(element)])
+
+        with st.expander("All ao5"):
+            for element in list_ao5:
+                st.write(element)
+        
+        with st.expander("All ao12"):
+            for element in list_ao12:
+                st.write(element)
+
+
+
+except:
+    st.error("Please go to home page first")
