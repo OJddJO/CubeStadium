06af5ce6168c64ef3e19cf9aa69005acf31bd008
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-01-15 13:24:42 +0100

    test

--- pages/3_Stats.py
+++ pages/3_Stats.py
@@ -37,7 +37,7 @@ try:
 
         timesExpander = st.expander("All times")
         for element in times:
-            timesExpander.caption(f"{element[0]}: {scrambles[times.index(element)]}")
+            timesExpander.caption(f"{element}: {scrambles[times.index(element)]}")
 
         ao5Expander = st.expander("All ao5")
         for element in list_ao5:
